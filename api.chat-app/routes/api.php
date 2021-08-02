41182af31ac68daa0bf7bd407cbe482754c0c9a4
WahyuS002 <syahputrawahyu61@gmail.com>
2021-08-03 06:33:08 +0700

    removed register logic in laravel

--- api.chat-app/routes/api.php
+++ api.chat-app/routes/api.php
@@ -4,8 +4,6 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\{ MeController, UserController, ConversationController };
 use App\Http\Controllers\Auth\AuthController;
 
-
-Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
 Route::group(['middleware' => 'auth:sanctum'], function(){
