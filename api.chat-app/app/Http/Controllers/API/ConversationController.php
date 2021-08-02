64f2cd00cd524dc21da072ef2f82ea9490f8e2f1
WahyuS002 <syahputrawahyu61@gmail.com>
2021-08-03 06:34:12 +0700

    deleted messageCreated event

--- api.chat-app/app/Http/Controllers/API/ConversationController.php
+++ api.chat-app/app/Http/Controllers/API/ConversationController.php
@@ -32,8 +32,6 @@ class ConversationController extends Controller
              'user_id' => auth()->user()->id,
          ]);
 
-         MessageCreated::dispatch($message);
-
          return response()->json([
              'data' => $message,
              'status' => 'Success'
